c4227fc0b831ad3288e8bc892048049132d90766
timwis <tim@timwis.com>
2015-11-15 18:17:18 -0500

    Don't show results until analyzed

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 var Vue = require('vue')
-var _ = { values: require('lodash/object/values') }
+var _ = { keys: require('lodash/object/keys'), values: require('lodash/object/values') }
 var analyze = require('./analyze')
 var exportSql = require('./export-sql')
 require('bootstrap/js/dropdown')
 
 new Vue({
-	el: 'body',
+	el: '#main',
+	template: '#template',
 	data: function() {
 		return {
 			fields: {},
+			fieldCount: null,
 			rowCount: null
 		}
 	},
@@ -18,6 +20,7 @@ new Vue({
 			var file = e.target.files.length ? e.target.files[0] : null
 			analyze(file).done(function(analysis) {
 				context.fields = analysis.fields
+				context.fieldCount = _.keys(analysis.fields).length
 				context.rowCount = analysis.rowCount
 			})
 		},
